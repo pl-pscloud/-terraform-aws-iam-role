3e103e0403b134dd27cd232413b5931f284392c0
pswitala <p.switala@pslab.eu>
2020-11-23 23:24:20 +0100

    Fixed tags and added service role path

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 resource "aws_iam_role" "pscloud-role" {
   name = "${var.pscloud_company}_iam_role_${var.pscloud_env}_${var.pscloud_role_name}"
 
+  path               = var.pscloud_service_role_path
   assume_role_policy = var.pscloud_assume_policy
 
   tags = {
-    tag-key = "tag-value"
+    Name = "${var.pscloud_company}_iam_role_${var.pscloud_env}_${var.pscloud_role_name}"
   }
 }
--- vars.tf
+++ vars.tf
@@ -2,5 +2,6 @@ variable "pscloud_env" {}
 variable "pscloud_company" {}
 
 variable "pscloud_role_name" {}
+variable "pscloud_service_role_path" {}
 
 variable "pscloud_assume_policy" {}
